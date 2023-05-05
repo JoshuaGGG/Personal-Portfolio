fe379e1999a1d95d2691585c64d2ae13005d6f0d
JoshuaGGG <joshua.guzman@student.cart.org>
2023-05-04 22:50:48 -0700

    style
    style

--- script.js
+++ script.js
@@ -23,57 +23,29 @@ logo.addEventListener("animationend", () =>{
     logo.classList.remove("bounce");
 });
 
+const swiper = new Swiper('.swiper', {
+  autoplay:{
+    delay:3000,
+    disableOnInjection:false,
+  },
+  effect: 'cube',
+  cubeEffect: {
+    slideShadows: false,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
 
-const slides = document.querySelectorAll('.slide');
-const btns = document.querySelectorAll('.btn');
-let currentSlide =1;
-
-const manualNav = function(manual){
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-    btns.forEach((btn) => {
-      btn.classList.remove('active');
-    });
-  });
-  slides[manual].classList.add('active');
-  btns[manual].classList.add('active');
-}
-
-btns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    manualNav(i);
-    currentSlide = i;
-  }
-  
-  )
-
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
-
-const repeat = function (activeClass){
-  let active =document.getElementsByClassName('active');
-  let i = 1;
-
-  const repeater = () =>{
-    setTimeout(function(){
-      [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove('active');
-      })
-      slides[i].classList.add('active');
-      btns[i].classList.add('active');
-      i++;
-
-      if(slides.length == i){
-        i = 0;
-      }
-      if(i >= slides.length ){
-        return;
-      }
-      repeater();
-    }, 10000)
-  }
-  repeater();
-}
-repeat();
-
 
 
